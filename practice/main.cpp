97c508f987ad978d4a15666e3e77e74d88c3f1c3
Heinrich <genatsaka@gmail.com>
2024-07-05 14:21:07 +0300

    exe created

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    size_t number_count, bin_count;
+    cout << "Enter number count: ";
+    cin >> number_count;
+    vector<double> numbers(number_count);
+    for(int i = 0; i < number_count; i++){
+        cin >> numbers[i];
+    }
+    cout << "Enter bin's count: ";
+    cin >> bin_count;
+    vector<size_t> bins(bin_count);
+
+    double min = numbers[0];
+    double max = numbers[0];
+    for (double x : numbers) {
+        if (x < min) {
+            min = x;
+        }
+        else if (x > max) {
+            max = x;
+        }
+    }
+    double bin_size = (max - min) / bin_count;
+    for (size_t i = 0; i < number_count; i++) {
+        bool found = false;
+        for (size_t j = 0; (j < bin_count - 1) && !found; j++) {
+            auto lo = min + j * bin_size;
+            auto hi = min + (j + 1) * bin_size;
+            if ((lo <= numbers[i]) && (numbers[i] < hi)) {
+                bins[j]++;
+                found = true;
+            }
+        }
+        if (!found) {
+            bins[bin_count - 1]++;
+        }
+    }
+    for(auto bin : bins){
+        for (int i = 0; i < bin; i++){
+            cout << '*';
+        }
+        cout << "| " << bin;
+        cout << endl;
+    }
 }
